3b3cccd96374148dc31425b3eac1bb4e0161b799
João Paulo <joaowais@gmail.com>
2023-08-20 23:06:44 -0300

    Primeira Requisição e Corrigindo Responsividade CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log('Sucesso!');
+
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+
+fetch(url).then((res) => {
+  console.log(res)
+});
+
+
+const x = 10 + 10;
+console.log(x);
